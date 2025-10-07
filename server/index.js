67affa7713c7a60ff70b40b5360149bf014644d6
J Brodie Day <jbrodieday@gmail.com>
2025-10-07 10:33:27 -0400

    refactor: code style update for server index

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import mysql from 'mysql2'
-import dotenv from 'dotenv'
-// Config dotenv to enable environment variables
-dotenv.config()
 
 // Initialize express instance
 const app = express()
+
+// Config dotenv to enable environment variables
+dotenv.config()
 
 const PORT = 3000
 
 // create db connection -> using a pool to allow multiple queries without terminating connections
 // Chaining the promise method to allow async functions instead of mysql callbacks
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  // password: process.env.DB_PW,
-  database: process.env.DB
-}).promise()
-
+const pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    // password: process.env.DB_PW,
+    database: process.env.DB,
+  })
+  .promise()
 
 // Retrieves table schema. Takes table name as string.
 const getSchema = async (tableName) => {
@@ -31,16 +33,21 @@ const getSchema = async (tableName) => {
   }
 }
 
-
 // Retrieves a single or set of resources from the game rules tables
 // Takes tableName STR, and optionally a columnName STR and columnValue STR:
-  // SELECT [columnName or *] FROM [tableName] (Optionally: WHERE columnName = columnValue)
+// SELECT [columnName or *] FROM [tableName] (Optionally: WHERE columnName = columnValue)
 const getResource = async (tableName, columnName, columnValue) => {
   // Select specific resource queries if a column name is specified
   if (columnName) {
     try {
       const result = await pool.query(
-        'SELECT * FROM' + pool.escapeId(tableName) + 'WHERE' + pool.escapeId(columnName) + '=' + pool.escape(columnValue))
+        'SELECT * FROM' +
+          pool.escapeId(tableName) +
+          'WHERE' +
+          pool.escapeId(columnName) +
+          '=' +
+          pool.escape(columnValue),
+      )
       return result[0]
     } catch (error) {
       console.error(error)
@@ -57,62 +64,69 @@ const getResource = async (tableName, columnName, columnValue) => {
   }
 }
 
-
 // Get table schema by table name -- for testing
-  // Spec: /schema/?table=<tableName>
+// Spec: /schema/?table=<tableName>
 app.get('/schema/', async (req, res) => {
   const query = req._parsedUrl.query
   const tableKeyStr = 'table='
   // URL must include query with table name
   if (!query || !query.includes(tableKeyStr)) {
-    const error = {message: 'No query provided or malformed query.'}
+    const error = { message: 'No query provided or malformed query.' }
     res.send(error)
     return
   }
   // Find query value by locating the `table` key
-  const queryValue = query.substring(query.indexOf(tableKeyStr) + tableKeyStr.length)
+  const queryValue = query.substring(
+    query.indexOf(tableKeyStr) + tableKeyStr.length,
+  )
   let result
 
   try {
     result = await getSchema(queryValue)
-    
   } catch (error) {
     result = error
   }
   res.send(result)
 })
 
-
 // Get all resources by table name (or get all columns for a specified row by table, column & value)
-  // Spec: /resource/?table=<tableValue>&<columnKey>=<columnValue>
+// Spec: /resource/?table=<tableValue>&<columnKey>=<columnValue>
 app.get('/resource/', async (req, res) => {
   const query = req.originalUrl
+  const tableKeyStr = 'table='
   const hasColumnQueryParam = query.includes('&')
   // Table name substring either ends at the start of the next query param (if it exists), else at end of url.
-  const tableQueryEndIndex = hasColumnQueryParam ? query.indexOf('&') : query.length
-  const tableValue = query.substring(query.indexOf('?table=') + '?table='.length, tableQueryEndIndex)
-  const columnQuery = query.substring(tableQueryEndIndex +1)
+  const tableQueryEndIndex = hasColumnQueryParam
+    ? query.indexOf('&')
+    : query.length
+  const tableValue = query.substring(
+    query.indexOf(tableKeyStr) + tableKeyStr.length,
+    tableQueryEndIndex,
+  )
+  const columnQuery = query.substring(tableQueryEndIndex + 1)
   const keyValSeparatorIndex = columnQuery.indexOf('=')
   // Column key and value can be passed as empty strings if there is no param present
-  const columnKey = hasColumnQueryParam ? columnQuery.substring(0, keyValSeparatorIndex) : ''
-  const columnValue = hasColumnQueryParam ? columnQuery.substring(keyValSeparatorIndex +1).replaceAll('+', ' ') : ''
+  const columnKey = hasColumnQueryParam
+    ? columnQuery.substring(0, keyValSeparatorIndex)
+    : ''
+  const columnValue = hasColumnQueryParam
+    ? columnQuery.substring(keyValSeparatorIndex + 1).replaceAll('+', ' ')
+    : ''
   let result
 
   try {
     result = await getResource(tableValue, columnKey, columnValue)
     // If no row exists with specified params, result will be empty array. Handle empty array case.
     if (result.length == 0) {
-      result = {message: 'No resource found.'}
+      result = { message: 'No resource found.' }
     }
-
   } catch (error) {
     result = error
   }
   res.send(result)
 })
 
-
-// Listen for connections on specified port. 
+// Listen for connections on specified port.
 app.listen(PORT, () => {
   console.log('Listening on port', PORT)
 })
